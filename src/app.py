89229f224cb04de770d1382053b34921cf93df67
Johan <jsruiza@eafit.edu.co>
2020-10-30 12:34:50 -0500

    agregado metodo de graficas y archivo html, aun no se ha probado

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ import db.DBController as db
 from flask import Flask, render_template
 from dotenv import load_dotenv
 from model.SIATA import SIATA
+import matplotlib.pyplot as plt
 
 load_dotenv(dotenv_path='..', verbose=True)
 app = Flask(__name__)
@@ -40,6 +41,28 @@ def get_estaciones():
         estaciones.append(estacion)
     print(estaciones)
     return render_template('estaciones.html', estaciones=estaciones, zonas=ciudades)
+
+@app.route('/graficas')
+def graficas():
+    db_estaciones = db.get_estaciones()
+    estaciones = []
+    ciudades = []
+    codigos = []
+    mediciones = []
+    for estacion in db_estaciones:
+        if estacion["ciudad"] not in ciudades:
+            ciudades.append(estacion["ciudad"])
+        estaciones.append(estacion) 
+    for estacion in estaciones:
+        codigos.append(estacion["codigo"])
+        medicion = estacion["mediciones"]
+        mediciones.append(medicion[0])
+    
+    grafica = plt.stem(codigos,mediciones)
+    plt.xlabel('codigo')
+    plt.ylabel('pm2.5')
+    plt.savefig("Grafica.jpg", bbox_inches='tight')
+    return render_template('graficas.html', estaciones=estaciones, zonas=ciudades)
 
 @app.route('/<int:codigo>')
 def render(codigo):
